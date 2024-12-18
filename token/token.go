0c6dc84cb57b79d7fa86708b671253d6328be099
phaseharry <harrychenhc@icloud.com>
2024-12-17 20:10:03 -0500

    [1.2] Define initial supported tokens

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LEFT_PAREN  = "("
+	RIGHT_PAREN = ")"
+
+	LEFT_BRACE  = "{"
+	RIGHT_BRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
